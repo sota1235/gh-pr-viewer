12437641387dad9c1eba4294a81ce5acd04f7e17
sota1235 <sota1235@gmail.com>
2018-06-30 23:09:48 +0900

    modify: test ajax

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,34 @@
-export default () => (
-  <div>Welcome to next.js!</div>
-);
+import axios from 'axios';
+import React from 'react';
+
+export default class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: '',
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://google.com')
+      .then(({ data }) => {
+        this.state.set({
+          data,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          { 'Welcome to next.js!' }
+        </div>
+        <div>
+          { this.state.data }
+        </div>
+      </div>
+    );
+  }
+}
